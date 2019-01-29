601c75d8ae218d32ec5f0ef96f312fc00b31854f
JEFFERSON FRAGOSO DE SOUZA <jeffersonfragoso@outlook.com>
2019-01-29 01:24:58 -0200

    refatorar: Redenrização dinâmica dos livros, conforme as prateleiras (shelfs), no componente BookList.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <BookList />;
+        <BookList books={this.state.books}/>
       </div>
     )
   }
