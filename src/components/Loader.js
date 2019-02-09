c23213c1ec3c24de84a808ec05c12f0d6d172a21
JEFFERSON FRAGOSO DE SOUZA <jeffersonfragoso@outlook.com>
2019-02-09 01:18:27 -0200

    refatorar : (Revision #1) - ajustes CSS

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactLoading from 'react-loading'
 
 const Loader = () => (
-    <ReactLoading type="bubbles" color="#46a3f4" height={'10%'} width={'10%'} />
+    <ReactLoading type="bubbles" color="#4380b6" height={'10%'} width={'10%'} />
 );
 
 export default Loader;
